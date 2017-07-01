7b023b027a88ed5d69daeb9569c54e35814eb03d
Aaron Bassett <me@aaronbassett.com>
2017-07-01 14:54:33 +0100

    Improve clientside error handling

--- static/js/click2call.js
+++ static/js/click2call.js
@@ -6,25 +6,38 @@ $(document).ready(function(){
 
   $('#send').click(function() {
     $('.notification').addClass('is-hidden')
+    $('input').removeClass('is-danger')
 
     var data = {
       'name': $('#name').val(),
       'number': $('#number').val()
     }
 
-    $('.control').addClass('is-disabled')
+    if(data.name && data.number) {
+      $('.control').addClass('is-disabled')
 
-    $.post('/call', JSON.stringify(data), function(response){
-      $('.control').removeClass('is-disabled')
+      $.post('/call', JSON.stringify(data), function(response){
+        $('.control').removeClass('is-disabled')
 
-      if(response.status === "started") {
-        $('#notification-number').html(data.number)
-        $('#notification-success').removeClass('is-hidden')
-        $('#name').val("")
-        $('#number').val("")
-      } else {
+        if(response.status === "started") {
+          $('#notification-number').html(data.number)
+          $('#notification-success').removeClass('is-hidden')
+          $('#name').val("")
+          $('#number').val("")
+        } else {
+          $('#notification-failure').removeClass('is-hidden')
+        }
+      }).fail(function() {
         $('#notification-failure').removeClass('is-hidden')
+      })
+    } else {
+      // missing a 1 or more required values
+      if(!$('#name').val()) {
+        $('#name').addClass('is-danger')
       }
-    })
+      if(!$('#number').val()) {
+        $('#number').addClass('is-danger')
+      }
+    }
   })
 })
